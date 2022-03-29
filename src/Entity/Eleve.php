125520a5b36b94f1fd8e02f770b16fd83b433e11
MateoB91 <mateobianco91100@gmail.com>
2022-03-29 11:18:40 +0200

    Reglement interieur + entity

---
+++ src/Entity/Eleve.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EleveRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EleveRepository::class)]
+class Eleve
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $prenom;
+
+    #[ORM\Column(type: 'date')]
+    private $datedenaissance;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDatedenaissance(): ?\DateTimeInterface
+    {
+        return $this->datedenaissance;
+    }
+
+    public function setDatedenaissance(\DateTimeInterface $datedenaissance): self
+    {
+        $this->datedenaissance = $datedenaissance;
+
+        return $this;
+    }
+}
